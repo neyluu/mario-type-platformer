42b263f49dacebb64bd08bfc3cd13da12d47eed9
neyluu <dwa2321@gmail.com>
2024-02-21 18:35:34 +0100

    basic side movement

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D playerRigidBody;
+    [SerializeField] private GameObject groundCheck;
+    [SerializeField] private float moveSpeed = 8f;
+    [SerializeField] private float jumpPower = 16f;
+    [SerializeField] private bool isFacingRight = true;
+    
+    private float horizontalMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        Debug.Log(horizontalMove);
+    }
+
+    private void FixedUpdate()
+    {
+        playerRigidBody.velocity = new Vector2(horizontalMove * moveSpeed, playerRigidBody.velocity.y);
+    }
+
+}
